2900b9e13d27c9e0ae9c44cb20d7c7ecfb17fe99
pis0sion <zihuang2010@126.com>
2022-08-30 16:41:24 +0800

    compose cs  commit @pis0sion

--- app/api/config/email.php
+++ app/api/config/email.php
@@ -6,5 +6,5 @@ return [
     'password' => 'VVMXIQXRAWRESFUQ', // 授权码
     'port' => '25', // 服务器端口
     'fromEmail' => 'lq198910065533@163.com', // 邮箱
-    'fromUserName' => 'Iris'
+    'fromUserName' => 'YXGSHOP'
 ];
