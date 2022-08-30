7d541b716357bdd2b9c16d0cb6e687443c175c1f
pis0sion <zihuang2010@126.com>
2022-08-30 09:48:42 +0800

    compose cs  commit @pis0sion

--- app/api/controller/v1/EmailController.php
+++ app/api/controller/v1/EmailController.php
@@ -27,6 +27,7 @@ class EmailController
         Cache::set($toEmail, $verifyCode, 20 * 60);
         $content = "Hello, welcome to register 【YXG Store】. Your email verification code is " . rand(100000, 999999) . ", please do not disclose it, beware of being scammed.";
         $this->send($toEmail, "email verification", $content);
+        
         return renderResponse();
     }
 
